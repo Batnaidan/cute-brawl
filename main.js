52e15bdc0cb0903eef6e8df2eefe32d246f4f857
Batnaidan Tsend <batnaidan@gmail.com>
2021-07-20 16:52:31 +0800

    Added Cannon-es implementation, force and fixed collision detection and adjusted camera and

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 //   MapControls,
 //   OrbitControls,
 // } from 'three/examples/jsm/controls/OrbitControls';
-
+import { World, Vec3, Box, Body, Plane, Sphere } from 'cannon-es';
 import nipplejs from 'nipplejs';
 import './style.css';
 
 const SPEED = 15;
-const HitBoxSideThreshold = 3.2;
+const TIMESTEP = 1 / 60;
+const CAMERAOFFSET = { z: 6 };
+const DAMPING = 0.5;
+const HitBoxSideThreshold = 10;
 const CollisionSideThreshold = 2.3;
 let renderer, scene, camera, joyStickInput;
 let prevTime = performance.now();
@@ -46,6 +49,10 @@ let characterControls = {
 joyStick.on('move', (event, data) => {
   joyStickInput = data;
   if (model1) {
+    // model1.animations.stand.reset();
+    model1.animations.stand.stop();
+    model1.animations.runAndPunch.play();
+    model1.animations.runAndPunch.setLoop(THREE.LoopRepeat);
     model1.scene.rotation.y = data.angle.radian + Math.PI / 2;
     velocity = findTriangleSide(data.angle.degree, data.distance);
     // model1.position.x += velocity.adjacent / 200;
@@ -57,6 +64,7 @@ joyStick.on('end', (event, data) => {
   if (model1) {
     velocity = null;
   }
+  model1.animations.stand.play();
   model1.animations.runAndPunch.stop();
 });
 
@@ -80,16 +88,52 @@ camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 5;
-camera.position.y = 5;
+camera.position.z = CAMERAOFFSET.z;
+camera.position.y = 7;
 
 // let cameraControls = new PointerLockControls(camera, renderer.domElement);
 
 let terrain = new THREE.GridHelper(100, 20, 0x0a0a0a, 0x0a0a0a);
-terrain.position.set(0, -0.5, 0);
+terrain.position.set(0, 0, 0);
 scene.add(terrain);
 
+let world = new World({
+  gravity: new Vec3(0, -10, 0),
+  allowSleep: false,
+});
+
+const groundBody = new Body({
+  type: Body.STATIC, // can also be achieved by setting the mass to 0
+  shape: new Plane(),
+});
+
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0); // make it face up
+world.addBody(groundBody);
+
+const radius = 4;
+// let box = new Body({
+//   mass: 2,
+//   shape: new Box(new Vec3(radius / 2, radius / 2, radius / 2)),
+// });
+let box = new Body({
+  mass: 2,
+  shape: new Sphere(radius / 2),
+});
+box.position.set(5, radius, 0);
+world.addBody(box);
+
+let modelBox = new Body({ mass: 10, shape: new Box(new Vec3(0.6, 1, 0.6)) });
+// modelBox.position.set(-2, (Math.random() - 0.5) * 1 + 2, 0);
+modelBox.position.set(0, 1, 0);
+world.addBody(modelBox);
+
+const geometry = new THREE.SphereGeometry(radius / 2);
+const material = new THREE.MeshNormalMaterial();
+const sphereMesh = new THREE.Mesh(geometry, material);
+scene.add(sphereMesh);
+
 let mixer,
+  mixer2,
   animations = {};
 
 function loadModel(url) {
@@ -99,41 +143,55 @@ function loadModel(url) {
 }
 
 let model1, model2;
-let p1 = loadModel('./resources/model/modelv2_stand.glb').then((result) => {
-  model1 = result;
-  mixer = new THREE.AnimationMixer(model1.scene);
-  model1.animations.stand = mixer.clipAction(model1.animations[10]);
-  model1.animations.stand.play();
-  model1.scene.rotation.y = Math.PI;
+let p1 = loadModel('./resources/model/modelv2_stand_with_punchv2.glb').then(
+  (result) => {
+    console.log(result);
+    model1 = result;
+    mixer = new THREE.AnimationMixer(model1.scene);
+    model1.scene.rotation.y = Math.PI;
 
-  camera.lookAt(model1.scene.position);
-  loadModel('./resources/model/modelv2_run_with_punch.glb').then((runModel) => {
-    model1.animations.runAndPunch = mixer.clipAction(runModel.animations[0]);
-  });
-});
+    model1.animations.stand = mixer.clipAction(model1.animations[10]);
+    model1.animations.stand.clampWhenFinished = true;
+    model1.animations.stand.setLoop(THREE.LoopRepeat);
+    model1.animations.stand.clampWhenFinished = true;
+    camera.lookAt(model1.scene.position);
+    loadModel('./resources/model/modelv2_run_with_punchv5.glb').then(
+      (runModel) => {
+        console.log(runModel);
+        model1.animations.runAndPunch = mixer.clipAction(
+          runModel.animations[0]
+        );
+        model1.animations.runAndPunch.clampWhenFinished = true;
+        model1.animations.runAndPunch.setLoop(THREE.LoopRepeat);
+        model1.animations.stand.play();
+      }
+    );
+  }
+);
 let p2 = loadModel('./resources/model/modelv2_stand.glb').then((result) => {
   model2 = result;
-  mixer = new THREE.AnimationMixer(model2.scene);
+  mixer2 = new THREE.AnimationMixer(model2.scene);
   model2.animations.stand = mixer.clipAction(model2.animations[10]);
+  model2.animations.stand.clampWhenFinished = true;
   model2.scene.rotation.y = Math.PI;
-  model2.animations.stand.play();
 
-  camera.lookAt(model2.scene.position);
-  loadModel('./resources/model/modelv2_run_with_punch.glb').then((runModel) => {
-    model2.animations.runAndPunch = mixer.clipAction(runModel.animations[0]);
-  });
+  loadModel('./resources/model/modelv2_run_with_punchv3.glb').then(
+    (runModel2) => {
+      model2.animations.runAndPunch = mixer.clipAction(runModel2.animations[0]);
+    }
+  );
 });
 
 Promise.all([p1, p2]).then(() => {
-  console.log(model1);
-  console.log(model2);
-  model1.scene.position.set(0, 0, 0);
-  model1.animations.stand.play();
-  model2.scene.position.set(0, 0, -5);
-  model2.animations.stand.play();
+  // model1.scene.position.set(0, 0, 0);
+
+  model2.scene.position.set(-5, 0, 0);
   //add model to the scene
   scene.add(model1.scene);
   scene.add(model2.scene);
+  window.setInterval(() => {
+    checkCollision();
+  }, 1000);
 });
 
 function updateMixer(deltaTime) {
@@ -149,26 +207,30 @@ function render() {
 
   const time = performance.now();
   const delta = (time - prevTime) / 1000;
-  // console.log(delta);
+
+  world.step(TIMESTEP, delta);
+  sphereMesh.position.copy(box.position);
+  sphereMesh.quaternion.copy(box.quaternion);
   if (mixer) updateMixer(delta);
   if (model1) {
+    model1.scene.position.copy(modelBox.position);
+    // camera.position.x = modelBox.position.x;
+    // camera.position.z = modelBox.position.z + CAMERAOFFSET.z;
     movePlayer(delta);
-    checkCollision();
   }
-  // let idealPosition = calculateCameraPosition();
-  // let idealLookAt = calculateCameraLook();
-
   prevTime = time;
   renderer.render(scene, camera);
 }
 
 function movePlayer(delta) {
   if (velocity) {
-    model1.animations.runAndPunch.play();
-    model1.scene.position.x += (velocity.adjacent / SPEED) * delta;
-    model1.scene.position.z += -(velocity.opposite / SPEED) * delta;
-    camera.position.x += (velocity.adjacent / SPEED) * delta;
-    camera.position.z -= (velocity.opposite / SPEED) * delta;
+    // model1.scene.position.x += (velocity.adjacent / SPEED) * delta;
+    // model1.scene.position.z += -(velocity.opposite / SPEED) * delta;
+    modelBox.position.x += (velocity.adjacent / SPEED) * delta;
+    modelBox.position.z += -(velocity.opposite / SPEED) * delta;
+    camera.position.x = modelBox.position.x;
+    camera.position.z = modelBox.position.z + CAMERAOFFSET.z;
+    // camera.position.z -= (velocity.opposite / SPEED) * delta;
   }
 }
 function pushEnemyAway() {}
@@ -177,10 +239,11 @@ function checkCollision() {
   // console.log(model1pos);
   // console.log(model1.scene.position);
   // let model2pos = (calculateFourSide(model2.scene.position);
-  let pos = model2.scene.position;
+  let pos = sphereMesh.position;
+  console.log;
   if (model1pos[0].x < pos.x && model1pos[1].x > pos.x) {
     if (model1pos[0].z > pos.z && model1pos[1].z > pos.z) {
-      console.log('collision');
+      applyForce();
     }
   }
   // console.log('collision');
@@ -209,4 +272,19 @@ function calculateFourSide({ x, z }) {
   });
   return hitBoxGrid;
 }
-function calculatePunchRange(x, z) {}
+function calculateOppositeAndAdjacent(x1, z1, x2, z2) {}
+
+function applyForce() {
+  box.linearDamping = DAMPING;
+  box.angularDamping = DAMPING;
+  let a = box.position.x - modelBox.position.x;
+  let b = box.position.z - modelBox.position.z;
+  console.log(box.position.x, modelBox.position.x);
+  a = a > 5 ? 5 : a;
+  b = b > 5 ? 5 : b;
+  // let c = Math.sqrt(a*a + b*b);
+  console.log(a, b);
+  const force = new Vec3(a * 100, 0, b * 100);
+  console.log(force);
+  box.applyForce(force);
+}
